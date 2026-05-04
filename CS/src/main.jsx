5ebbbcdab90ad71019b8370542614a0b377310d6
Vasanthi <vasanthibojjagani21@gmail.com>
2026-05-04 18:38:11 +0530

    Persona Updated

--- CS/src/main.jsx
+++ CS/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from "./App.jsx";
 import './index.css'
-import ThemeProvider from './components/theme-provider.jsx';
+import {ThemeProvider} from './components/theme-provider.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <ThemeProvider>
